955a21e3743291c8eff2e8877012d903ed528bf2
Sergio-Luis20 <sergioluisdasilvaruiz05@gmail.com>
2023-03-20 10:47:59 -0300

    fixed bug at toArray method and added null returns if arrays are null

--- src/main/java/br/sergio/utils/Utils.java
+++ src/main/java/br/sergio/utils/Utils.java
@@ -109,6 +109,9 @@ public final class Utils {
      */
 
     public static byte[] toPrimitive(Byte[] array) {
+        if(array == null) {
+            return null;
+        }
     	byte[] newArray = new byte[array.length];
     	for(int i = 0; i < array.length; i++) {
     		newArray[i] = array[i];
@@ -117,6 +120,9 @@ public final class Utils {
     }
 
     public static short[] toPrimitive(Short[] array) {
+        if(array == null) {
+            return null;
+        }
     	short[] newArray = new short[array.length];
     	for(int i = 0; i < array.length; i++) {
     		newArray[i] = array[i];
@@ -125,6 +131,9 @@ public final class Utils {
     }
 
     public static int[] toPrimitive(Integer[] array) {
+        if(array == null) {
+            return null;
+        }
     	int[] newArray = new int[array.length];
     	for(int i = 0; i < array.length; i++) {
     		newArray[i] = array[i];
@@ -133,6 +142,9 @@ public final class Utils {
     }
 
     public static long[] toPrimitive(Long[] array) {
+        if(array == null) {
+            return null;
+        }
     	long[] newArray = new long[array.length];
     	for(int i = 0; i < array.length; i++) {
     		newArray[i] = array[i];
@@ -141,6 +153,9 @@ public final class Utils {
     }
 
     public static float[] toPrimitive(Float[] array) {
+        if(array == null) {
+            return null;
+        }
     	float[] newArray = new float[array.length];
     	for(int i = 0; i < array.length; i++) {
     		newArray[i] = array[i];
@@ -149,6 +164,9 @@ public final class Utils {
     }
 
     public static double[] toPrimitive(Double[] array) {
+        if(array == null) {
+            return null;
+        }
     	double[] newArray = new double[array.length];
     	for(int i = 0; i < array.length; i++) {
     		newArray[i] = array[i];
@@ -157,6 +175,9 @@ public final class Utils {
     }
 
     public static boolean[] toPrimitive(Boolean[] array) {
+        if(array == null) {
+            return null;
+        }
     	boolean[] newArray = new boolean[array.length];
     	for(int i = 0; i < array.length; i++) {
     		newArray[i] = array[i];
@@ -165,6 +186,9 @@ public final class Utils {
     }
 
     public static char[] toPrimitive(Character[] array) {
+        if(array == null) {
+            return null;
+        }
     	char[] newArray = new char[array.length];
     	for(int i = 0; i < array.length; i++) {
     		newArray[i] = array[i];
@@ -173,6 +197,9 @@ public final class Utils {
     }
 
     public static Byte[] toWrapper(byte[] array) {
+        if(array == null) {
+            return null;
+        }
     	Byte[] newArray = new Byte[array.length];
     	for(int i = 0; i < array.length; i++) {
     		newArray[i] = array[i];
@@ -181,6 +208,9 @@ public final class Utils {
     }
 
     public static Short[] toWrapper(short[] array) {
+        if(array == null) {
+            return null;
+        }
     	Short[] newArray = new Short[array.length];
     	for(int i = 0; i < array.length; i++) {
     		newArray[i] = array[i];
@@ -189,6 +219,9 @@ public final class Utils {
     }
 
     public static Integer[] toWrapper(int[] array) {
+        if(array == null) {
+            return null;
+        }
     	Integer[] newArray = new Integer[array.length];
     	for(int i = 0; i < array.length; i++) {
     		newArray[i] = array[i];
@@ -197,6 +230,9 @@ public final class Utils {
     }
     
     public static Long[] toWrapper(long[] array) {
+        if(array == null) {
+            return null;
+        }
     	Long[] newArray = new Long[array.length];
     	for(int i = 0; i < array.length; i++) {
     		newArray[i] = array[i];
@@ -205,6 +241,9 @@ public final class Utils {
     }
     
     public static Float[] toWrapper(float[] array) {
+        if(array == null) {
+            return null;
+        }
     	Float[] newArray = new Float[array.length];
     	for(int i = 0; i < array.length; i++) {
     		newArray[i] = array[i];
@@ -213,6 +252,9 @@ public final class Utils {
     }
     
     public static Double[] toWrapper(double[] array) {
+        if(array == null) {
+            return null;
+        }
     	Double[] newArray = new Double[array.length];
     	for(int i = 0; i < array.length; i++) {
     		newArray[i] = array[i];
@@ -221,6 +263,9 @@ public final class Utils {
     }
     
     public static Boolean[] toWrapper(boolean[] array) {
+        if(array == null) {
+            return null;
+        }
     	Boolean[] newArray = new Boolean[array.length];
     	for(int i = 0; i < array.length; i++) {
     		newArray[i] = array[i];
@@ -229,6 +274,9 @@ public final class Utils {
     }
     
     public static Character[] toWrapper(char[] array) {
+        if(array == null) {
+            return null;
+        }
     	Character[] newArray = new Character[array.length];
     	for(int i = 0; i < array.length; i++) {
     		newArray[i] = array[i];
@@ -237,20 +285,37 @@ public final class Utils {
     }
 
     public static <T> List<T> toList(T[] array) {
+        if(array == null) {
+            return null;
+        }
         return new ArrayList<>(Arrays.asList(array));
     }
 
     public static <T> Set<T> toSet(T[] array) {
+        if(array == null) {
+            return null;
+        }
         return new LinkedHashSet<>(Arrays.asList(array));
     }
 
     public static <T> Queue<T> toQueue(T[] array) {
+        if(array == null) {
+            return null;
+        }
         return new LinkedList<>(Arrays.asList(array));
     }
 
     @SuppressWarnings("unchecked")
     public static <T> T[] toArray(Collection<T> collection) {
-        return (T[]) collection.toArray();
+        if(collection == null) {
+            return null;
+        }
+        T[] array = (T[]) new Object[collection.size()];
+        int i = 0;
+        for(T obj : collection) {
+            array[i++] = obj;
+        }
+        return array;
     }
     
     public static Object parseNumberArray(String input, Class<?> componentType) {
