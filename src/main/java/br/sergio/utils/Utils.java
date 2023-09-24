7c56c09004ad482ac8e821135f37327276e10d58
Sergio-Luis20 <sergioluisdasilvaruiz05@gmail.com>
2023-09-24 17:19:26 -0300

    added more reflection utilities to Utils

--- src/main/java/br/sergio/utils/Utils.java
+++ src/main/java/br/sergio/utils/Utils.java
@@ -1,11 +1,13 @@
 package br.sergio.utils;
 
 import java.lang.reflect.Array;
+import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.LinkedHashSet;
 import java.util.LinkedList;
 import java.util.List;
@@ -13,6 +15,7 @@ import java.util.Queue;
 import java.util.Set;
 import java.util.function.Function;
 import java.util.function.Predicate;
+import java.util.stream.Stream;
 
 public final class Utils {
 	
@@ -365,31 +368,269 @@ public final class Utils {
     public static boolean isNumericPrimitive(Class<?> clazz) {
         return isPrimitive(clazz) && clazz != boolean.class && clazz != char.class;
     }
+    
+    public static boolean isNumericIntegerPrimitive(Class<?> clazz) {
+    	return isNumericPrimitive(clazz) && clazz != float.class && clazz != double.class;
+    }
+    
+    public static boolean isNumericRealPrimitive(Class<?> clazz) {
+    	return isNumericPrimitive(clazz) && !isNumericIntegerPrimitive(clazz);
+    }
 
     public static boolean isPrimitive(Class<?> clazz) {
-        return clazz == byte.class ||
-               clazz == short.class ||
-               clazz == int.class ||
-               clazz == long.class ||
-               clazz == float.class ||
-               clazz == double.class ||
-               clazz == boolean.class ||
-               clazz == char.class;
+        return clazz.isPrimitive();
+    }
+    
+    public static Class<?> getPrimitiveClass(Class<?> wrapper) {
+    	if(wrapper == Byte.class) {
+    		return byte.class;
+    	} else if(wrapper == Short.class) {
+    		return short.class;
+    	} else if(wrapper == Integer.class) {
+    		return int.class;
+    	} else if(wrapper == Long.class) {
+    		return long.class;
+    	} else if(wrapper == Float.class) {
+    		return float.class;
+    	} else if(wrapper == Double.class) {
+    		return double.class;
+    	} else if(wrapper == Boolean.class) {
+    		return boolean.class;
+    	} else if(wrapper == Character.class) {
+    		return char.class;
+    	} else {
+    		return null;
+    	}
+    }
+    
+    public static Class<?> getPrimitiveCheckingClass(Class<?> wrapper) {
+    	if(isPrimitive(wrapper)) {
+    		return wrapper;
+    	}
+    	return getPrimitiveClass(wrapper);
     }
 
     public static boolean isNumericWrapper(Class<?> clazz) {
         return isWrapper(clazz) && clazz != Boolean.class && clazz != Character.class;
     }
+    
+    public static boolean isNumericIntegerWrapper(Class<?> clazz) {
+    	return isNumericWrapper(clazz) && clazz != Float.class && clazz != Double.class;
+    }
+    
+    public static boolean isNumericRealWrapper(Class<?> clazz) {
+    	return isNumericWrapper(clazz) && !isNumericIntegerWrapper(clazz);
+    }
 
     public static boolean isWrapper(Class<?> clazz) {
-        return clazz == Byte.class ||
-               clazz == Short.class ||
-               clazz == Integer.class ||
-               clazz == Long.class ||
-               clazz == Float.class ||
-               clazz == Double.class ||
-               clazz == Boolean.class ||
-               clazz == Character.class;
+        return getPrimitiveClass(clazz) != null;
+    }
+    
+    public static Class<?> getWrapperClass(Class<?> primitive) {
+    	if(primitive == byte.class) {
+    		return Byte.class;
+    	} else if(primitive == short.class) {
+    		return Short.class;
+    	} else if(primitive == int.class) {
+    		return Integer.class;
+    	} else if(primitive == long.class) {
+    		return Long.class;
+    	} else if(primitive == float.class) {
+    		return Float.class;
+    	} else if(primitive == double.class) {
+    		return Double.class;
+    	} else if(primitive == boolean.class) {
+    		return Boolean.class;
+    	} else if(primitive == char.class) {
+    		return Character.class;
+    	} else {
+    		return null;
+    	}
+    }
+    
+    public static Class<?> getWrapperCheckingClass(Class<?> primitive) {
+    	if(isWrapper(primitive)) {
+    		return primitive;
+    	}
+    	return getWrapperClass(primitive);
+    }
+    
+    public static Object mapWrapperConverting(Class<?> clazz, String value) {
+    	return mapWrapper(getWrapperCheckingClass(clazz), value);
+    }
+    
+    public static Object mapWrapper(Class<?> clazz, String value) {
+    	if(!isWrapper(clazz)) {
+			throw new IllegalArgumentException("Not a wrapper class: " + clazz.getName());
+		}
+		if(clazz == Character.class) {
+			if(value.length() != 1) {
+				throw new IllegalArgumentException("Class: " + clazz.getName() + ", String length: " + value.length() + ", allowed: 1");
+			}
+			return Character.valueOf(value.toCharArray()[0]);
+		} else {
+			try {
+				return clazz.getMethod("valueOf", String.class).invoke(null, value);
+			} catch(Exception e) {					
+				// Should not get here since our verifications
+				throw new Error(e);
+			}
+		}
+    }
+
+    public static boolean isNumeric(Class<?> clazz) {
+        return isNumericPrimitive(clazz) || isNumericWrapper(clazz);
+    }
+
+    public static boolean isNumericInteger(Class<?> clazz) {
+        return isNumericIntegerPrimitive(clazz) || isNumericIntegerWrapper(clazz);
+    }
+
+    public static boolean isNumericReal(Class<?> clazz) {
+        return isNumericRealPrimitive(clazz) || isNumericRealWrapper(clazz);
+    }
+    
+    public static List<Field> getNonStaticFields(Class<?> clazz) {
+    	return getNonStaticFields(clazz, false);
+    }
+    
+    public static List<Field> getNonStaticFields(Class<?> clazz, boolean accessiblesOnly) {
+    	return Stream.of(accessiblesOnly ? clazz.getFields() : clazz.getDeclaredFields())
+    			.filter(field -> !Modifier.isStatic(field.getModifiers())).toList();
+    }
+    
+    public static List<Field> getAllFields(Class<?> clazz) {
+    	return getAllFields(clazz, false);
+    }
+    
+    public static List<Field> getAllFields(Class<?> clazz, boolean accessiblesOnly) {
+    	// Collect fields from subclasses to superclasses
+		List<List<Field>> fields = new ArrayList<>();
+		while(clazz != Object.class) {
+			fields.add(getNonStaticFields(clazz, accessiblesOnly));
+			clazz = clazz.getSuperclass();
+		}
+		/*
+		 * Checks for duplicates in superclasses, keeping
+		 * fields from subclasses and deleting those from
+		 * superclasses 
+		 */
+		int size = fields.size();
+		for(int i = 0; i < size - 1; i++) {
+			List<Field> list = fields.get(i);
+			for(Field field : list) {
+				for(int j = i + 1; j < size; j++) {
+					List<Field> superList = fields.get(j);
+					for(Field superField : superList) {
+						if(equalsField(field, superField)) {
+							superList.remove(superField);
+						}
+					}
+				}
+			}
+		}
+		/*
+		 * Sort fields from superclasses to subclasses
+		 * and return a list consisting of a flatMap of them
+		 */
+		Collections.reverse(fields);
+		return fields.stream().flatMap(List::stream).toList();
+	}
+    
+    private static boolean equalsField(Field a, Field b) {
+    	return a.getName().equals(b.getName());
+    }
+    
+    public static List<Method> getNonStaticMethods(Class<?> clazz) {
+    	return getNonStaticMethods(clazz, false);
+    }
+    
+    public static List<Method> getNonStaticMethods(Class<?> clazz, boolean accessiblesOnly) {
+    	return Stream.of(accessiblesOnly ? clazz.getMethods() : clazz.getDeclaredMethods())
+				.filter(method -> !Modifier.isStatic(method.getModifiers())).toList();
+    }
+    
+    public static List<Method> getAllMethods(Class<?> clazz) {
+    	return getAllMethods(clazz, false);
+    }
+    
+    public static List<Method> getAllMethods(Class<?> clazz, boolean accessiblesOnly) {
+    	// Collect methods from subclasses to superclasses
+    	List<List<Method>> methods = new ArrayList<>();
+		while(clazz != Object.class) {
+			methods.add(Stream.of(accessiblesOnly ? clazz.getMethods() : clazz.getDeclaredMethods())
+					.filter(method -> !Modifier.isStatic(method.getModifiers())).toList());
+			clazz = clazz.getSuperclass();
+		}
+		/*
+		 * Checks for duplicates in superclasses, keeping
+		 * methods from subclasses and deleting those from
+		 * superclasses 
+		 */
+		int size = methods.size();
+		for(int i = 0; i < size - 1; i++) {
+			List<Method> list = methods.get(i);
+			for(Method method : list) {
+				for(int j = i + 1; j < size; j++) {
+					List<Method> superList = methods.get(j);
+					for(Method superMethod : superList) {
+						if(equalsMethod(method, superMethod)) {
+							superList.remove(superMethod);
+						}
+					}
+				}
+			}
+		}
+		/*
+		 * Sort methods from superclasses to subclasses
+		 * and return a list consisting of a flatMap of them
+		 */
+		Collections.reverse(methods);
+		return methods.stream().flatMap(List::stream).toList();
+    }
+    
+    private static boolean equalsMethod(Method a, Method b) {
+    	return a.getName().equals(b.getName()) && Arrays.equals(a.getParameterTypes(), b.getParameterTypes());
+    }
+    
+    public static Object defaultInitValue(Class<?> clazz) {
+    	if(clazz == byte.class || clazz == Byte.class) return (byte) 0;
+    	if(clazz == short.class || clazz == Short.class) return (short) 0;
+    	if(clazz == int.class || clazz == Integer.class) return 0;
+    	if(clazz == long.class || clazz == Long.class) return 0L;
+    	if(clazz == float.class || clazz == Float.class) return 0.0f;
+    	if(clazz == double.class || clazz == Double.class) return 0.0;
+    	if(clazz == boolean.class || clazz == Boolean.class) return false;
+    	if(clazz == char.class || clazz == Character.class) return (char) 0;
+    	return null;
+    }
+
+    public static boolean areEquivalent(Class<?> c1, Class<?> c2) {
+        if(c1 == c2) {
+            return true;
+        }
+        if(c1.isPrimitive() && getPrimitiveCheckingClass(c2) == c1) {
+            return true;
+        }
+        if(isWrapper(c1) && getWrapperCheckingClass(c2) == c1) {
+            return true;
+        }
+        return false;
+    }
+
+    public static boolean isBasic(Class<?> c) {
+        return c.isPrimitive() || isWrapper(c) || c == String.class;
+    }
+
+    public static String methodToString(Method method) {
+        String name = method.getName();
+		Class<?>[] params = method.getParameterTypes();
+		if(params.length == 0) {
+			return name + "()";
+		}
+		String paramsString = Arrays.toString(Stream.of(params).map(Class::getName).toArray());
+		paramsString = paramsString.substring(1, paramsString.length() - 1);
+		return name + "(" + paramsString + ")";
     }
     
     public static String toTextLines(List<String> lines) {
