c2e9cdd8ca0ab6606b13637e070260d3120fc4a7
Sergio-Luis20 <sergioluisdasilvaruiz05@gmail.com>
2024-04-14 22:38:05 -0300

    added utility default methods to TriFunction, TriConsumer and TriPredicate

--- src/main/java/br/sergio/utils/TriConsumer.java
+++ src/main/java/br/sergio/utils/TriConsumer.java
@@ -1,8 +1,18 @@
 package br.sergio.utils;
 
+import java.util.Objects;
+
 @FunctionalInterface
 public interface TriConsumer<A, B, C> {
     
     void accept(A a, B b, C c);
+
+    default TriConsumer<A, B, C> andThen(TriConsumer<? super A, ? super B, ? super C> other) {
+        Objects.requireNonNull(other);
+        return (a, b, c) -> {
+            accept(a, b, c);
+            other.accept(a, b, c);
+        };
+    }
 
 }
--- src/main/java/br/sergio/utils/TriFunction.java
+++ src/main/java/br/sergio/utils/TriFunction.java
@@ -1,8 +1,16 @@
 package br.sergio.utils;
 
+import java.util.Objects;
+import java.util.function.Function;
+
 @FunctionalInterface
 public interface TriFunction<A, B, C, D> {
     
     D apply(A a, B b, C c);
+
+    default <E> TriFunction<A, B, C, E> andThen(Function<D, E> after) {
+        Objects.requireNonNull(after);
+        return (a, b, c) -> after.apply(apply(a, b, c));
+    }
 
 }
